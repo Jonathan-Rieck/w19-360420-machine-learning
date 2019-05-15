70033716533fdd63688d1e77b97a0aff66db1f1a
jonathan Rieck <jonathanrieck123@gmail.com>
2019-05-14 20:05:01 -0400

    updates

--- ErrorAnalysis.md
+++ ErrorAnalysis.md
@@ -9,3 +9,13 @@
 ## Distributions of Model Accuracy
 
 ## Analysis of different error types
+# Amount of Confidence
+'1) different accuracy'
+	The accurancy varies from one run to another due to random number generations, which creates different random numbers at every time the program is ran. 
+'2) Baseline'
+	A sensible baseline is about  
+# Types of Errors
+'1) False Positive & False Negative'
+	A false positive, is when a doctor diagnoses a healthy individual as a sick one. While a false negative is when a doctor diagnoses a sick patient a healthy one.
+'2) Baseline with Recall & Precision'
+		A sensible baseline to ensure everyone's safety and health is to assign everyone as sick, which will ensure that recall and precision acheive maximum scores.
--- kNNMain.java
+++ kNNMain.java
@@ -9,7 +9,9 @@ public class kNNMain
 {
 //to enable the different print statements
 public final static boolean task5 = false;       //to enable task5 enter true, otherwise false
-public final static boolean task6 = true;        //to enable task6 enter true, otherwise false
+public final static boolean task6 = false;       //to enable task6 enter true, otherwise false
+public final static boolean taskRun = true;      //to enable n number of runs enter true, otherwise false
+public final static int n = 1000;				 //number of runs
 
   public static void main(String... args) throws FileNotFoundException{
 
@@ -17,7 +19,7 @@ public final static boolean task6 = true;        //to enable task6 enter true, o
     // the desired file. Choose a given DataPoint, and print its features and label
 
 		List<DataPoint> data = new ArrayList<DataPoint>();
-		String file = "C:\\Users\\Jonathan Rieck\\Desktop\\College\\4th Semester\\Programing\\Java Code\\Flower Lab\\src\\breastCancer.csv";
+		String file = "C:\\Users\\Jonathan Rieck\\Desktop\\College\\4th Semester\\Programing\\Java Code\\Flower Lab\\src\\breastCancer.csv.csv";
 		data = DataSet.readDataSet(file);
 		DataPoint list= data.get(14);
 		
@@ -26,7 +28,7 @@ public final static boolean task6 = true;        //to enable task6 enter true, o
 			System.out.println("Label " + list.getLabel()+ " "+printablelist);
 
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
-		double fraction = .25;
+		double fraction = .3;
 		List<DataPoint> training = new ArrayList<DataPoint>();
 		List<DataPoint> test = new ArrayList<DataPoint>();
 		training = DataSet.getTrainingSet(data, fraction);  
@@ -53,7 +55,6 @@ public final static boolean task6 = true;        //to enable task6 enter true, o
 
     // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
     // point based on nearest neighbors in training set. Calculate accuracy of model.
-<<<<<<< HEAD
 		if (task6)
 		{
 		//the accuracy of the the model
@@ -72,41 +73,109 @@ public final static boolean task6 = true;        //to enable task6 enter true, o
 				  sum ++;	
 				}
 			}
-			System.out.print(sum/training.size() * 100);
+			System.out.print(sum/training.size() * 100 + "%");
 		}//end task 6
+		
+	// 									1000 Runs
+	// _____________________________________________________________________________
+		
+		if (taskRun)
+		{
+			double [] indScore = new double [n];					//individual score one each run 
+			double sumInd = 0.; 									//Individual sum of each run
+			double sumTotal = 0.; 									// sum of all right answers
+			String [] wrong = new String [n*n];     					//collection of all wrong predictions
+			int k = 0;												// number of wrong entries
+			int false_positive = 0;									//Number of false positives								
+			int false_negative = 0; 								//Number of false negatives
+			int malignant = 0;										//Number of malignant
+			int benign = 0;											//Number of Benign 
+			double precision =0.;
+			double recall = 0.;
+			
+			//looping a 1000 times over the array 
+				for ( int j = 0; j < n ; j++)
+				{
+					sumInd = 0.; 
+				//Looping over all values of the array of data points of the test set
+					for ( int i=0; i< training.size(); i++ )
+					{
+						KNNClassifier set = new KNNClassifier (4);
+						DataPoint point = test.get(i);
+						String label = point.getLabel();
+						String prediction = set.predict(training, point);
+						
+						if (label.equals(prediction))
+						{
+						  sumInd ++;	
+						  sumTotal ++;
+						}
+						if (!label.equals(prediction))
+						{
+						  wrong[k] = prediction;
+						  k++;
+						}
+						if(prediction.equals("malignant"))
+						{
+							malignant++;
+						}
+						else
+						{
+							benign++; 
+						}
+						indScore[j] = (sumInd/training.size() * 100);
+					}
+				}
+				System.out.println((sumTotal/training.size()/n) * 100 + "% after 1000 runs");
+				double mean = mean(indScore);
+				System.out.println(mean + "% is the mean score");
+				System.out.println(standDev(indScore, mean) + "is the standard deviation");
+				
+			//Later tasks
+				System.out.println("BASELINE:");
+				DataSet.printLabelFrequencies(training); 
+				System.out.println("Total malignant: " + malignant + " Total benign: " +benign);
+				
+				
+				for (int i = 0; i<k;i++)
+				{
+					String b = wrong[i] ;
+					if (b.equals("benign"))
+					{
+						false_negative++;
+					}
+					else
+					{
+						false_positive++;
+					}
+				}
+				System.out.println("total false positives: "+ false_positive + "\ntotal false negatives: " +false_negative+ "\ntotal wrong: "+ k);
+				
+				precision = malignant/(false_positive + malignant);
+				recall = malignant/(false_negative + malignant);
+				
+				System.out.println("Precision: " + precision);
+				System.out.println("Recall: " + recall );
+				
+		}
   }//end main class
+  
+  public static double mean(double [] arr)
+  {
+	  double sum = 0; 
+	  for (int i = 0; i < n; i++)
+	  {
+		  sum = sum + arr[i];
+	  }
+	  return sum/1000; 
+  }
+  public static double standDev(double [] arr, double m)
+  { 
+	  double sum = 0;
+	  for (int i = 0; i <n; i++)
+	  {
+		  sum = Math.pow(m-arr[i], 2);
+	  }
+	  return Math.sqrt(sum/n); 
+  }
 }//end class
-=======
-
-
-  }
-
-  public static double mean(double[] arr){
-    /*
-    Method that takes as an argument an array of doubles
-    Returns: average of the elements of array, as a double
-    */
-    double sum = 0.0;
-
-    for (double a : arr){
-      sum += a;
-    }
-    return (double)sum/arr.length;
-  }
-
-  public static double standardDeviation(double[] arr){
-    /*
-    Method that takes as an argument an array of doubles
-    Returns: standard deviation of the elements of array, as a double
-    Dependencies: requires the *mean* method written above
-    */
-    double avg = mean(arr);
-    double sum = 0.0;
-    for (double a : arr){
-      sum += Math.pow(a-avg,2);
-    }
-    return (double)sum/arr.length;
-  }
-
-}
->>>>>>> ac8b64ec5ad33c35bb9f6ee98f2b36d7a3b0f37c
